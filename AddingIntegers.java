f1685b5f86d9ba33b19b3623d9fa4e45aad20aa4
bulentsahinkayas <54605607+bulentsahinkayas@users.noreply.github.com>
2019-08-27 20:36:20 -0400

    Adding Numbers

---
+++ AddingIntegers.java
@@ -0,0 +1,17 @@
+package FirstWeekDay2;
+
+public class AddingIntegers {
+    public static void main(String[]args){
+        int number1=12;
+        int number2=35;
+        int theSum=number1+number2;
+        System.out.println("The sum of integers:"+theSum);
+        double number3=8;
+        double number4=5;
+        double division=number3/number4;
+        System.out.println("The division of two numbers:"+division);
+        boolean firstBoolean=true;
+        System.out.println(firstBoolean);
+
+    }
+}
